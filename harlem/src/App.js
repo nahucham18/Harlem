084fd0f5de5e6ceae66a5f423d8a3b3885254961
nahucham18 <nahu.chamo19@gmail.com>
2023-03-05 18:35:57 -0300

    Cracion de app

---
+++ harlem/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src='https://menu.harlemburgas.com.ar/assets/img/logo.png?v=2' alt='harlem'/>
+    </div>
+  );
+}
+
+export default App;
