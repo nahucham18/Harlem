f0b7750a2e38848e2af88e9f34b257876eacd8ca
nahucham18 <nahu.chamo19@gmail.com>
2023-03-21 14:43:34 -0300

    Incorporando la libreria de iconos de react

--- harlem/src/components/Status/Status.jsx
+++ harlem/src/components/Status/Status.jsx
@@ -1,5 +1,7 @@
 import style from './Status.module.css'
 import { Link } from 'react-router-dom'
+import {FaHamburger} from 'react-icons/fa'
+import {ImLocation2} from 'react-icons/im'
 
 const Status = () =>{
     return (
@@ -7,15 +9,21 @@ const Status = () =>{
             <h2 className={style.section}>ESTAMOS CERRADOS</h2>
             <p className={style.sub_section}>Nuesto horario de atención es: Miércoles a Domingos de 20:30 a23:30</p>
             <hr />
-            <h2 className={style.section}>NUESTRO MENÚ</h2>
-            <Link className={style.enlace} to={`/menu`}>
-                <button className={style.btn} >Ver</button>
-            </Link>
-            <hr />
-            <h2 className={style.section}>ZONAS DE ENTREGA</h2>
-            <Link to={`https://menu.harlemburgas.com.ar/info_zonas.php`}>
-                <button className={style.btn} >Ver</button>
-            </Link>
+           <div className={style.option}>
+             <h2 className={style.section}>NUESTRO MENÚ</h2>
+             <Link className={style.enlace} to={`/menu`}>
+                 <button className={style.btn} ><FaHamburger/></button>
+             </Link>
+           </div>
+            
+
+            <div className={style.option}>
+                <h2 className={style.section}>ZONAS DE ENTREGA</h2>
+                <Link to={`https://menu.harlemburgas.com.ar/info_zonas.php`}>
+                    <button className={style.btn} ><ImLocation2/></button>
+                </Link>
+            </div>
+
         </div>
     )
 }
