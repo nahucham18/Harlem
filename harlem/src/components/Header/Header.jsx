8e9819254d5905b2dfbf1b7b28cb0dcfedd3f868
nahucham18 <nahu.chamo19@gmail.com>
2023-04-02 19:05:55 -0300

    Componente Promociones

--- harlem/src/components/Header/Header.jsx
+++ harlem/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import style from './Header.module.css'
 import { Link } from 'react-router-dom'
 import Status from '../Status/Status'
+import Promociones from '../Promociones/Promociones'
 
 const Header = ( ) =>{
     return (
@@ -9,6 +10,7 @@ const Header = ( ) =>{
                 <img className={style.logo} src='https://menu.harlemburgas.com.ar/assets/img/logo.png?v=2' alt='harlem'/>
             </Link>
             <Status/>
+            <Promociones/>
         </header>
     )
 }
--- harlem/src/components/Promociones/Promociones.jsx
+++ harlem/src/components/Promociones/Promociones.jsx
@@ -3,8 +3,10 @@ import style from './Promociones.module.css'
 const Promociones = () =>{
     return (
         <div className={style.container}>
-            <h2>PROMOCIONES</h2>
-            <p>Mira las promocione que tenemos para vos!</p>
+            <div className={style.divPromociones}>
+                <h2 className={style.title}>PROMOCIONES</h2>
+                <p>Mira las promocione que tenemos para vos!</p>
+            </div>
         </div>
     )
 }
